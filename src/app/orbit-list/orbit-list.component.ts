bdbe5e01b7bce54665a8b14943e99b494df49c8d
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-01-06 04:57:42 -0500

    passed in satellites to orbit list component

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Satellite } from "../satellite";
 @Component({
-  selector: 'app-orbit-list',
-  templateUrl: './orbit-list.component.html',
-  styleUrls: ['./orbit-list.component.css']
+  selector: "app-orbit-list",
+  templateUrl: "./orbit-list.component.html",
+  styleUrls: ["./orbit-list.component.css"]
 })
 export class OrbitListComponent implements OnInit {
+  @Input() satellites: Satellite[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
